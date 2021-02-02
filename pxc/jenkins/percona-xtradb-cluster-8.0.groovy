b5b8707633fbd81d6474cea2c8c7fb425dbe7c2b
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2021-02-02 17:14:18 +0200

    test pxc-80 job

--- pxc/jenkins/percona-xtradb-cluster-8.0.groovy
+++ pxc/jenkins/percona-xtradb-cluster-8.0.groovy
@@ -1,13 +1,13 @@
-library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+library changelog: false, identifier: 'lib@pxc80test', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/vorsel/jenkins-pipelines.git'
 ]) _
 
 void buildStage(String DOCKER_OS, String STAGE_PARAM) {
     sh """
         set -o xtrace
-        mkdir test
-        wget https://raw.githubusercontent.com/percona/percona-xtradb-cluster/${GIT_BRANCH}/build-ps/pxc_builder.sh -O pxc_builder.sh
+        mkdir -p test
+        wget https://raw.githubusercontent.com/vorsel/percona-xtradb-cluster/${GIT_BRANCH}/build-ps/pxc_builder.sh -O pxc_builder.sh
         pwd -P
         ls -laR
         export build_dir=\$(pwd -P)
@@ -29,11 +29,11 @@ def AWS_STASH_PATH
 
 pipeline {
     agent {
-        label 'docker'
+        label 'docker-32gb'
     }
     parameters {
         string(
-            defaultValue: 'https://github.com/percona/percona-xtradb-cluster.git',
+            defaultValue: 'https://github.com/vorsel/percona-xtradb-cluster.git',
             description: 'URL for percona-xtradb-cluster repository',
             name: 'GIT_REPO')
         string(
@@ -70,11 +70,11 @@ pipeline {
     stages {
         stage('Create PXC source tarball') {
             steps {
-                slackNotify("#releases", "#00FF00", "[${JOB_NAME}]: starting build for ${GIT_BRANCH}")
+                slackNotify("@alex.miroshnychenko", "#00FF00", "[${JOB_NAME}]: starting build for ${GIT_BRANCH}")
                 cleanUpWS()
                 buildStage("centos:7", "--get_sources=1")
                 sh '''
-                   REPO_UPLOAD_PATH=$(grep "UPLOAD" test/pxc-80.properties | cut -d = -f 2 | sed "s:$:${BUILD_NUMBER}:")
+                   REPO_UPLOAD_PATH=$(grep "DEST=UPLOAD" test/pxc-80.properties | cut -d = -f 2 | sed "s:$:${BUILD_NUMBER}:")
                    AWS_STASH_PATH=$(echo ${REPO_UPLOAD_PATH} | sed  "s:UPLOAD/experimental/::")
                    echo ${REPO_UPLOAD_PATH} > uploadPath
                    echo ${AWS_STASH_PATH} > awsUploadPath
@@ -85,6 +85,7 @@ pipeline {
                 script {
                     AWS_STASH_PATH = sh(returnStdout: true, script: "cat awsUploadPath").trim()
                 }
+                stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
                 stash includes: 'uploadPath', name: 'uploadPath'
                 pushArtifactFolder("source_tarball/", AWS_STASH_PATH)
                 uploadTarballfromAWS("source_tarball/", AWS_STASH_PATH, 'source')
@@ -94,174 +95,199 @@ pipeline {
             parallel {
                 stage('Build PXC generic source rpm') {
                     agent {
-                        label 'docker'
+                        label 'docker-32gb'
                     }
                     steps {
                         cleanUpWS()
+                        unstash 'pxc-80.properties'
                         popArtifactFolder("source_tarball/", AWS_STASH_PATH)
                         buildStage("centos:7", "--build_src_rpm=1")
 
+                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
                         pushArtifactFolder("srpm/", AWS_STASH_PATH)
                         uploadRPMfromAWS("srpm/", AWS_STASH_PATH)
                     }
                 }
-                stage('Build PXC generic source deb') {
+/*                stage('Build PXC generic source deb') {
                     agent {
-                        label 'docker'
+                        label 'docker-32gb'
                     }
                     steps {
                         cleanUpWS()
+                        unstash 'pxc-80.properties'
                         popArtifactFolder("source_tarball/", AWS_STASH_PATH)
                         buildStage("ubuntu:xenial", "--build_source_deb=1")
 
+                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
                         pushArtifactFolder("source_deb/", AWS_STASH_PATH)
                         uploadDEBfromAWS("source_deb/", AWS_STASH_PATH)
                     }
                 }
+*/
             }  //parallel
         } // stage
         stage('Build PXC RPMs/DEBs/Binary tarballs') {
             parallel {
-                stage('Centos 7') {
+/*                stage('Centos 7') {
                     agent {
-                        label 'docker'
+                        label 'docker-32gb'
                     }
                     steps {
                         cleanUpWS()
+                        unstash 'pxc-80.properties'
                         popArtifactFolder("srpm/", AWS_STASH_PATH)
                         buildStage("centos:7", "--build_rpm=1")
 
+                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
                         pushArtifactFolder("rpm/", AWS_STASH_PATH)
                         uploadRPMfromAWS("rpm/", AWS_STASH_PATH)
                     }
                 }
                 stage('Centos 8') {
                     agent {
-                        label 'docker'
+                        label 'docker-32gb'
                     }
                     steps {
                         cleanUpWS()
+                        unstash 'pxc-80.properties'
                         popArtifactFolder("srpm/", AWS_STASH_PATH)
                         buildStage("centos:8", "--build_rpm=1")
 
+                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
                         pushArtifactFolder("rpm/", AWS_STASH_PATH)
                         uploadRPMfromAWS("rpm/", AWS_STASH_PATH)
                     }
                 }
                 stage('Ubuntu Xenial(16.04)') {
                     agent {
-                        label 'docker'
+                        label 'docker-32gb'
                     }
                     steps {
                         cleanUpWS()
+                        unstash 'pxc-80.properties'
                         popArtifactFolder("source_deb/", AWS_STASH_PATH)
                         buildStage("ubuntu:xenial", "--build_deb=1")
 
+                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
                         pushArtifactFolder("deb/", AWS_STASH_PATH)
                         uploadDEBfromAWS("deb/", AWS_STASH_PATH)
                     }
                 }
                 stage('Ubuntu Bionic(18.04)') {
                     agent {
-                        label 'docker'
+                        label 'docker-32gb'
                     }
                     steps {
                         cleanUpWS()
+                        unstash 'pxc-80.properties'
                         popArtifactFolder("source_deb/", AWS_STASH_PATH)
                         buildStage("ubuntu:bionic", "--build_deb=1")
 
+                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
                         pushArtifactFolder("deb/", AWS_STASH_PATH)
                         uploadDEBfromAWS("deb/", AWS_STASH_PATH)
                     }
                 }
                 stage('Ubuntu Focal(20.04)') {
                     agent {
-                        label 'docker'
+                        label 'docker-32gb'
                     }
                     steps {
                         cleanUpWS()
+                        unstash 'pxc-80.properties'
                         popArtifactFolder("source_deb/", AWS_STASH_PATH)
                         buildStage("ubuntu:focal", "--build_deb=1")
 
+                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
                         pushArtifactFolder("deb/", AWS_STASH_PATH)
                         uploadDEBfromAWS("deb/", AWS_STASH_PATH)
                     }
                 }
                 stage('Debian Stretch(9)') {
                     agent {
-                        label 'docker'
+                        label 'docker-32gb'
                     }
                     steps {
                         cleanUpWS()
+                        unstash 'pxc-80.properties'
                         popArtifactFolder("source_deb/", AWS_STASH_PATH)
                         buildStage("debian:stretch", "--build_deb=1")
 
+                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
                         pushArtifactFolder("deb/", AWS_STASH_PATH)
                         uploadDEBfromAWS("deb/", AWS_STASH_PATH)
                     }
                 }
                 stage('Debian Buster(10)') {
                     agent {
-                        label 'docker'
+                        label 'docker-32gb'
                     }
                     steps {
                         cleanUpWS()
+                        unstash 'pxc-80.properties'
                         popArtifactFolder("source_deb/", AWS_STASH_PATH)
                         buildStage("debian:buster", "--build_deb=1")
 
+                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
                         pushArtifactFolder("deb/", AWS_STASH_PATH)
                         uploadDEBfromAWS("deb/", AWS_STASH_PATH)
                     }
                 }
+*/
                 stage('Centos 7 tarball') {
                     agent {
-                        label 'docker'
+                        label 'docker-32gb'
                     }
                     steps {
                         cleanUpWS()
+                        unstash 'pxc-80.properties'
                         popArtifactFolder("source_tarball/", AWS_STASH_PATH)
                         buildStage("centos:7", "--build_tarball=1")
 
+                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
                         pushArtifactFolder("tarball/", AWS_STASH_PATH)
                         uploadTarballfromAWS("tarball/", AWS_STASH_PATH, 'binary')
                     }
                 }
                 stage('Centos 7 debug tarball') {
                     agent {
-                        label 'docker'
+                        label 'docker-32gb'
                     }
                     steps {
                         cleanUpWS()
+                        unstash 'pxc-80.properties'
                         popArtifactFolder("source_tarball/", AWS_STASH_PATH)
-                        buildStage("centos:7", "--debug=1")
+                        buildStage("centos:7", "--build_tarball=1 --debug=1")
 
+                        stash includes: 'test/pxc-80.properties', name: 'pxc-80.properties'
                         pushArtifactFolder("debug/", AWS_STASH_PATH)
                     }
                 }
             }
         }
 
-        stage('Sign packages') {
+/*        stage('Sign packages') {
             steps {
                 signRPM()
                 signDEB()
             }
         }
+
         stage('Push to public repository') {
             steps {
                 // sync packages
                 sync2ProdAutoBuild(PXC_REPO, COMPONENT)
             }
         }
-
+*/
     }
     post {
         success {
-            slackNotify("#releases", "#00FF00", "[${JOB_NAME}]: build has been finished successfully for ${GIT_BRANCH}")
+            slackNotify("@alex.miroshnychenko", "#00FF00", "[${JOB_NAME}]: build has been finished successfully for ${GIT_BRANCH}")
             deleteDir()
         }
         failure {
-            slackNotify("#releases", "#FF0000", "[${JOB_NAME}]: build failed for ${GIT_BRANCH}")
+            slackNotify("@alex.miroshnychenko", "#FF0000", "[${JOB_NAME}]: build failed for ${GIT_BRANCH}")
             deleteDir()
         }
         always {
