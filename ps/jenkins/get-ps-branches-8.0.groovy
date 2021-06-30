f65e2cf83e78c6a9f8871f42feece472f0054d1c
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2021-06-30 17:00:04 +0300

    test ps80 autobuild

--- ps/jenkins/get-ps-branches-8.0.groovy
+++ ps/jenkins/get-ps-branches-8.0.groovy
@@ -1,6 +1,6 @@
-library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+library changelog: false, identifier: 'lib@ps80_autobuild_TEST', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/vorsel/jenkins-pipelines.git'
 ]) _
 
 pipeline {
@@ -25,22 +25,22 @@ pipeline {
                 withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AWS_STASH', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                     sh """
                         EC=0
-                        aws s3 ls s3://percona-jenkins-artifactory/percona-server/branch_commit_id_80.properties || EC=\$?
+                        aws s3 ls s3://percona-jenkins-artifactory/percona-server/branch_commit_id_80-TEST.properties || EC=\$?
 
             if [ \${EC} = 1 ]; then
               LATEST_RELEASE_BRANCH=\$(git -c 'versionsort.suffix=-' ls-remote --heads --sort='v:refname' ${GIT_REPO} release-8.0\\* | tail -1)
               BRANCH_NAME=\$(echo \${LATEST_RELEASE_BRANCH} | cut -d "/" -f 3)
               COMMIT_ID=\$(echo \${LATEST_RELEASE_BRANCH} | cut -d " " -f 1)
 
-              echo "BRANCH_NAME=\${BRANCH_NAME}" > branch_commit_id_80.properties
-              echo "COMMIT_ID=\${COMMIT_ID}" >> branch_commit_id_80.properties
+              echo "BRANCH_NAME=\${BRANCH_NAME}" > branch_commit_id_80-TEST.properties
+              echo "COMMIT_ID=\${COMMIT_ID}" >> branch_commit_id_80-TEST.properties
 
-              aws s3 cp branch_commit_id_80.properties s3://percona-jenkins-artifactory/percona-server/
+              aws s3 cp branch_commit_id_80-TEST.properties s3://percona-jenkins-artifactory/percona-server/
                           echo "START_NEW_BUILD=NO" > startBuild
             else
-                          aws s3 cp s3://percona-jenkins-artifactory/percona-server/branch_commit_id_80.properties .
-              source ./branch_commit_id_80.properties
-              cat ./branch_commit_id_80.properties
+                          aws s3 cp s3://percona-jenkins-artifactory/percona-server/branch_commit_id_80-TEST.properties .
+              source ./branch_commit_id_80-TEST.properties
+              cat ./branch_commit_id_80-TEST.properties
 
               LATEST_RELEASE_BRANCH=\$(git -c 'versionsort.suffix=-' ls-remote --heads --sort='v:refname' ${GIT_REPO} release-8.0\\* | tail -1)
               LATEST_BRANCH_NAME=\$(echo \${LATEST_RELEASE_BRANCH} | cut -d "/" -f 3)
@@ -53,20 +53,20 @@ pipeline {
                 echo "START_NEW_BUILD=NO" > startBuild
               fi
 
-              echo "BRANCH_NAME=\${LATEST_BRANCH_NAME}" > branch_commit_id_80.properties
-              echo "COMMIT_ID=\${LATEST_COMMIT_ID}" >> branch_commit_id_80.properties
-              echo "VERSION=\${VERSION}" >> branch_commit_id_80.properties
-                          aws s3 cp branch_commit_id_80.properties s3://percona-jenkins-artifactory/percona-server/
+              echo "BRANCH_NAME=\${LATEST_BRANCH_NAME}" > branch_commit_id_80-TEST.properties
+              echo "COMMIT_ID=\${LATEST_COMMIT_ID}" >> branch_commit_id_80-TEST.properties
+              echo "VERSION=\${VERSION}" >> branch_commit_id_80-TEST.properties
+                          aws s3 cp branch_commit_id_80-TEST.properties s3://percona-jenkins-artifactory/percona-server/
                         fi
                     """
                 }
                 script {
                     START_NEW_BUILD = sh(returnStdout: true, script: "source ./startBuild; echo \${START_NEW_BUILD}").trim()
-                    BRANCH_NAME = sh(returnStdout: true, script: "source ./branch_commit_id_80.properties; echo \${BRANCH_NAME}").trim()
-                    COMMIT_ID = sh(returnStdout: true, script: "source ./branch_commit_id_80.properties; echo \${COMMIT_ID}").trim()
-                    VERSION = sh(returnStdout: true, script: "source ./branch_commit_id_80.properties; echo \${BRANCH_NAME} | cut -d - -f 2-3 ").trim()
-                    TOKUBACKUP_BRANCH = sh(returnStdout: true, script: "source ./branch_commit_id_80.properties; echo Percona-Server-\${VERSION}").trim()
-                    PERCONAFT_BRANCH = sh(returnStdout: true, script: "source ./branch_commit_id_80.properties; echo Percona-Server-\${VERSION}").trim()
+                    BRANCH_NAME = sh(returnStdout: true, script: "source ./branch_commit_id_80-TEST.properties; echo \${BRANCH_NAME}").trim()
+                    COMMIT_ID = sh(returnStdout: true, script: "source ./branch_commit_id_80-TEST.properties; echo \${COMMIT_ID}").trim()
+                    VERSION = sh(returnStdout: true, script: "source ./branch_commit_id_80-TEST.properties; echo \${BRANCH_NAME} | cut -d - -f 2-3 ").trim()
+                    TOKUBACKUP_BRANCH = sh(returnStdout: true, script: "source ./branch_commit_id_80-TEST.properties; echo Percona-Server-\${VERSION}").trim()
+                    PERCONAFT_BRANCH = sh(returnStdout: true, script: "source ./branch_commit_id_80-TEST.properties; echo Percona-Server-\${VERSION}").trim()
                 }
 
             }
@@ -82,8 +82,8 @@ pipeline {
                         echo ${START_NEW_BUILD}: build required
                     """
                 }
-                slackNotify("#releases", "#00FF00", "[${JOB_NAME}]: new changes for branch ${BRANCH_NAME}[commit id: ${COMMIT_ID}] were detected, build will be started soon")
-                build job: 'ps8.0-autobuild-RELEASE', parameters: [string(name: 'BRANCH', value: BRANCH_NAME), string(name: 'PERCONAFT_BRANCH', value: PERCONAFT_BRANCH), string(name: 'TOKUBACKUP_BRANCH', value: TOKUBACKUP_BRANCH), string(name: 'COMPONENT', value: 'testing')]
+                slackNotify("@alex.miroshnychenko", "#00FF00", "[${JOB_NAME}]: new changes for branch ${BRANCH_NAME}[commit id: ${COMMIT_ID}] were detected, build will be started soon")
+                build job: 'ps8.0-autobuild-RELEASE-test', parameters: [string(name: 'BRANCH', value: BRANCH_NAME), string(name: 'PERCONAFT_BRANCH', value: PERCONAFT_BRANCH), string(name: 'TOKUBACKUP_BRANCH', value: TOKUBACKUP_BRANCH), string(name: 'COMPONENT', value: 'testing')]
 
             }
         }
--- ps/jenkins/percona-server-for-mysql-8.0.groovy
+++ ps/jenkins/percona-server-for-mysql-8.0.groovy
@@ -1,7 +1,7 @@
 /* groovylint-disable DuplicateStringLiteral, GStringExpressionWithinString, LineLength */
-library changelog: false, identifier: 'lib@master', retriever: modernSCM([
+library changelog: false, identifier: 'lib@ps80_autobuild_TEST', retriever: modernSCM([
     $class: 'GitSCMSource',
-    remote: 'https://github.com/Percona-Lab/jenkins-pipelines.git'
+    remote: 'https://github.com/vorsel/jenkins-pipelines.git'
 ]) _
 
 void installCli(String PLATFORM) {
@@ -25,13 +25,17 @@ void installCli(String PLATFORM) {
 void buildStage(String DOCKER_OS, String STAGE_PARAM) {
     sh """
         set -o xtrace
-        mkdir test
+        mkdir -p test
         wget https://raw.githubusercontent.com/percona/percona-server/${BRANCH}/build-ps/percona-server-8.0_builder.sh -O ps_builder.sh || curl https://raw.githubusercontent.com/percona/percona-server/${BRANCH}/build-ps/percona-server-8.0_builder.sh -o ps_builder.sh
         pwd -P
         ls -laR
         export build_dir=\$(pwd -P)
         set -o xtrace
         cd \${build_dir}
+        pwd -P
+        if [ -f ./test/percona-server-8.0.properties ]; then
+            . ./test/percona-server-8.0.properties
+        fi
         sudo bash -x ./ps_builder.sh --builddir=\${build_dir}/test --install_deps=1
         bash -x ./ps_builder.sh --builddir=\${build_dir}/test --repo=${GIT_REPO} --branch=${BRANCH} --perconaft_branch=${PERCONAFT_BRANCH} --tokubackup_branch=${TOKUBACKUP_BRANCH} --rpm_release=${RPM_RELEASE} --deb_release=${DEB_RELEASE} ${STAGE_PARAM}
     """
@@ -75,7 +79,7 @@ parameters {
                label 'min-xenial-x64'
             }
             steps {
-                slackNotify("#releases", "#00FF00", "[${JOB_NAME}]: starting build for ${BRANCH}")
+                slackNotify("@alex.miroshnychenko", "#00FF00", "[${JOB_NAME}]: starting build for ${BRANCH}")
                 cleanUpWS()
                 installCli("deb")
                 buildStage("ubuntu:xenial", "--get_sources=1")
@@ -92,6 +96,7 @@ parameters {
                     AWS_STASH_PATH = sh(returnStdout: true, script: "cat awsUploadPath").trim()
                 }
                 stash includes: 'uploadPath', name: 'uploadPath'
+                stash includes: 'test/percona-server-8.0.properties', name: 'properties'
                 pushArtifactFolder("source_tarball/", AWS_STASH_PATH)
                 uploadTarballfromAWS("source_tarball/", AWS_STASH_PATH, 'source')
             }
@@ -105,6 +110,7 @@ parameters {
                     steps {
                         cleanUpWS()
                         installCli("rpm")
+                        unstash 'properties'
                         popArtifactFolder("source_tarball/", AWS_STASH_PATH)
                         buildStage("centos:6", "--build_src_rpm=1")
 
@@ -112,6 +118,7 @@ parameters {
                         uploadRPMfromAWS("srpm/", AWS_STASH_PATH)
                     }
                 }
+/*
                 stage('Build PS generic source deb') {
                     agent {
                         label 'min-xenial-x64'
@@ -119,6 +126,7 @@ parameters {
                     steps {
                         cleanUpWS()
                         installCli("deb")
+                        unstash 'properties'
                         popArtifactFolder("source_tarball/", AWS_STASH_PATH)
                         buildStage("ubuntu:xenial", "--build_source_deb=1")
 
@@ -126,6 +134,7 @@ parameters {
                         uploadDEBfromAWS("source_deb/", AWS_STASH_PATH)
                     }
                 }
+*/
             }  //parallel
         } // stage
         stage('Build PS RPMs/DEBs/Binary tarballs') {
@@ -137,6 +146,7 @@ parameters {
                     steps {
                         cleanUpWS()
                         installCli("rpm")
+                        unstash 'properties'
                         popArtifactFolder("srpm/", AWS_STASH_PATH)
                         buildStage("centos:7", "--build_rpm=1")
 
@@ -144,69 +154,14 @@ parameters {
                         uploadRPMfromAWS("rpm/", AWS_STASH_PATH)
                     }
                 }
-                stage('Centos 6') {
-                    agent {
-                        label 'min-centos-6-x64'
-                    }
-                    steps {
-                        cleanUpWS()
-                        installCli("rpm")
-                        popArtifactFolder("srpm/", AWS_STASH_PATH)
-                        buildStage("centos:6", "--build_rpm=1")
-
-                        pushArtifactFolder("rpm/", AWS_STASH_PATH)
-                        uploadRPMfromAWS("rpm/", AWS_STASH_PATH)
-                    }
-                }
-                stage('Centos 8') {
-                    agent {
-                        label 'min-centos-8-x64'
-                    }
-                    steps {
-                        cleanUpWS()
-                        installCli("rpm")
-                        popArtifactFolder("srpm/", AWS_STASH_PATH)
-                        buildStage("centos:8", "--build_rpm=1")
-
-                        pushArtifactFolder("rpm/", AWS_STASH_PATH)
-                        uploadRPMfromAWS("rpm/", AWS_STASH_PATH)
-                    }
-                }
-                stage('Ubuntu Xenial(16.04)') {
-                    agent {
-                        label 'min-xenial-x64'
-                    }
-                    steps {
-                        cleanUpWS()
-                        installCli("deb")
-                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
-                        buildStage("ubuntu:xenial", "--build_deb=1")
-
-                        pushArtifactFolder("deb/", AWS_STASH_PATH)
-                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
-                    }
-                }
-                stage('Ubuntu Bionic(18.04)') {
-                    agent {
-                        label 'min-bionic-x64'
-                    }
-                    steps {
-                        cleanUpWS()
-                        installCli("deb")
-                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
-                        buildStage("ubuntu:bionic", "--build_deb=1")
-
-                        pushArtifactFolder("deb/", AWS_STASH_PATH)
-                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
-                    }
-                }
-                stage('Ubuntu Focal(20.04)') {
+/*                stage('Ubuntu Focal(20.04)') {
                     agent {
                         label 'min-focal-x64'
                     }
                     steps {
                         cleanUpWS()
                         installCli("deb")
+                        unstash 'properties'
                         popArtifactFolder("source_deb/", AWS_STASH_PATH)
                         buildStage("ubuntu:focal", "--build_deb=1")
 
@@ -214,69 +169,44 @@ parameters {
                         uploadDEBfromAWS("deb/", AWS_STASH_PATH)
                     }
                 }
-                stage('Debian Stretch(9)') {
+                stage('Centos 7 binary tarball') {
                     agent {
-                        label 'min-stretch-x64'
-                    }
-                    steps {
-                        cleanUpWS()
-                        installCli("deb")
-                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
-                        buildStage("debian:stretch", "--build_deb=1")
-
-                        pushArtifactFolder("deb/", AWS_STASH_PATH)
-                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
-                    }
-                }
-                stage('Debian Buster(10)') {
-                    agent {
-                        label 'min-buster-x64'
-                    }
-                    steps {
-                        cleanUpWS()
-                        installCli("deb")
-                        popArtifactFolder("source_deb/", AWS_STASH_PATH)
-                        buildStage("debian:buster", "--build_deb=1")
-
-                        pushArtifactFolder("deb/", AWS_STASH_PATH)
-                        uploadDEBfromAWS("deb/", AWS_STASH_PATH)
-                    }
-                }
-                stage('Centos 6 binary tarball') {
-                    agent {
-                        label 'min-centos-6-x64'
+                        label 'min-centos-7-x64'
                     }
                     steps {
                         cleanUpWS()
                         installCli("rpm")
+                        unstash 'properties'
                         popArtifactFolder("source_tarball/", AWS_STASH_PATH)
-                        buildStage("centos:6", "--build_tarball=1")
+                        buildStage("centos:7", "--build_tarball=1")
 
                         pushArtifactFolder("tarball/", AWS_STASH_PATH)
                         uploadTarballfromAWS("tarball/", AWS_STASH_PATH, 'binary')
                     }
                 }
-                stage('Centos 6 debug tarball') {
+                stage('Centos 7 debug tarball') {
                     agent {
-                        label 'min-centos-6-x64'
+                        label 'min-centos-7-x64'
                     }
                     steps {
                         cleanUpWS()
                         installCli("rpm")
+                        unstash 'properties'
                         popArtifactFolder("source_tarball/", AWS_STASH_PATH)
-                        buildStage("centos:6", "--debug=1 --build_tarball=1")
+                        buildStage("centos:7", "--debug=1 --build_tarball=1")
 
                         pushArtifactFolder("tarball/", AWS_STASH_PATH)
                         uploadTarballfromAWS("tarball/", AWS_STASH_PATH, 'binary')
                     }
                 }
+*/
             }
         }
 
         stage('Sign packages') {
             steps {
                 signRPM()
-                signDEB()
+//                signDEB()
             }
         }
         stage('Push to public repository') {
@@ -289,11 +219,11 @@ parameters {
     }
     post {
         success {
-            slackNotify("#releases", "#00FF00", "[${JOB_NAME}]: build has been finished successfully for ${BRANCH}")
+            slackNotify("@alex.miroshnychenko", "#00FF00", "[${JOB_NAME}]: build has been finished successfully for ${BRANCH}")
             deleteDir()
         }
         failure {
-            slackNotify("#releases", "#FF0000", "[${JOB_NAME}]: build failed for ${BRANCH}")
+            slackNotify("@alex.miroshnychenko", "#FF0000", "[${JOB_NAME}]: build failed for ${BRANCH}")
             deleteDir()
         }
         always {
